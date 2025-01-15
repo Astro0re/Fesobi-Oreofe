62e754ac5acfc20282b336dd2b5d091f071f7d4a
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-01-15 23:20:24 +0100

    Update

--- Scripting.js
+++ Scripting.js
@@ -1,8 +1,8 @@
 var skill_Con = document.getElementsByClassName("Sk_Con");
 
 
-skill_Con.onclick = function on_hover(){
-    heig
+skill_Con.onmouseover = function on_hover(){
+    ResizeObserverSize
 } 
 skill_Con.onmouseover = function on_hover(){
     heig
