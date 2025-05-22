df815b2ef49def342945f02c90e5a784cd0fdf08
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-05-22 20:04:15 +0100

    File Replaced and component created

---
+++ src/components/Edu_Exp.jsx
@@ -0,0 +1,18 @@
+export default function ed_ex(){
+    return(
+        <div>
+            <div>
+				<p>Bachelor of Science Degree in Anatomy</p>
+				<p>Babcock University, Nigeria</p>
+				<p>2019-2023</p>
+		    </div>
+            <div>
+                <h6>Html/css</h6>
+                <h6>Python</h6>
+                <h6>R</h6>
+                <h6>SQL</h6>
+                <h6>Power Bi</h6>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/skills.jsx
@@ -0,0 +1,7 @@
+export default function skills(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
