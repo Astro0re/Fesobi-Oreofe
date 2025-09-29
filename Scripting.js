6157d3dc0e22c4ccb7dd50baccaf6d691a937644
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-29 16:13:51 +0100

    Naming fix

--- Scripting.js
+++ Scripting.js
@@ -4,6 +4,7 @@ function cert_display_IBM(){
         <h5>Certification In Data Fundamentals from IBM SkillsBuild</h5>
         <h5>Certification In Big Data Fundamentals from IBM SkillsBuild</h5> 
         <h5>Certification In Artificail Intelligence Fundamentals from IBM SkillsBuild</h5>
+        <a href="Certification Display.html">View Here.</a>
         `;
 }
 
@@ -12,20 +13,25 @@ function cert_display_KAGG(){
 			<h5>Certification in Introduction to Programming from Kaggle Learn</h5>
 			<h5>Certification in Python from Kaggle Learn</h5>
 			<h5>Certification in SQL from Kaggle Learn</h5>
-        `;
+            <a href="Certification Display.html">View Here.</a>
+            <a href="Certification Display.html">View Here.</a>
+            `;
 }
 function cert_display_ALX(){
     document.getElementById('Cer_dis').innerHTML = `
 			<h5>Certification in Ai Career Essentials from ALX</h5>
-        `;
+            <a href="Certification Display.html">View Here.</a>
+            `;
 }
 function cert_display_SAN(){
     document.getElementById('Cer_dis').innerHTML = `
 			<h5>Certification in High-Performance Leadership from Santander</h5>
-        `;
+            <a href="Certification Display.html">View Here.</a>
+            `;
 }
 function cert_display_AWS(){
     document.getElementById('Cer_dis').innerHTML = `
                 <h5>Certification in Data Engineering from AWS Skills Build </h5>
-        `;
+                <a href="Certification Display.html">View Here.</a>
+                `;
 }
