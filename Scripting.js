afe89c6a52b012cdeefe9fd040f2eff6fac51a3e
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-09-29 12:06:00 +0100

    Function update

--- Scripting.js
+++ Scripting.js
@@ -1,33 +1,31 @@
-var skill_Con = document.getElementsByClassName("Sk_Con");
-// Changing color of background on hover/periodicaly
-document.getElementByID("Face").on_hover.StyleSheet="background-color: blue;";
-// merry-go-round
-document.getElementByID("Skill_Page")
-document.getElementByID("Courses")
-
-skill_Con.onmouseover = function on_hover(){
-} 
-skill_Con.onmouseover = function on_hover(){
-    
-} 
-onclick.skill_Con = function click(){
-    StyleSheet
-}
-
-var Java_Con = document.getElementById("Java").innerHTML ;
-Java_Con = function on_hover() {
-    
-}
-
-function cert_display(){
-    icon = document.getElementById('Cer_dis')
-    if (document.getElementById("IBM") === true) {
-        icon.innerHTML = `
+// Certification Display Function
+function cert_display_IBM(){
+    document.getElementById('Cer_dis').innerHTML = `
         <h5>Certification In Data Fundamentals from IBM SkillsBuild</h5>
         <h5>Certification In Big Data Fundamentals from IBM SkillsBuild</h5> 
         <h5>Certification In Artificail Intelligence Fundamentals from IBM SkillsBuild</h5>
         `;
-    }
 }
 
-document.getElementsByClassName('Cer_I').onclick = cert_display
+function cert_display_KAGG(){
+    document.getElementById('Cer_dis').innerHTML = `
+			<h5>Certification in Introduction to Programming from Kaggle Learn</h5>
+			<h5>Certification in Python from Kaggle Learn</h5>
+			<h5>Certification in SQL from Kaggle Learn</h5>
+        `;
+}
+function cert_display_ALX(){
+    document.getElementById('Cer_dis').innerHTML = `
+			<h5>Certification in Ai Career Essentials from ALX</h5>
+        `;
+}
+function cert_display_SAN(){
+    document.getElementById('Cer_dis').innerHTML = `
+			<h5>Certification in High-Performance Leadership from Santander</h5>
+        `;
+}
+function cert_display_AWS(){
+    document.getElementById('Cer_dis').innerHTML = `
+                <h5>Certification in Data Engineering from AWS Skills Build </h5>
+        `;
+}
