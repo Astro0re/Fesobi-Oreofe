29f22bf20ceedc90e20ff6559b939f39d54a9e55
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-11-04 08:36:20 +0100

    personal data

---
+++ data.tsx
@@ -0,0 +1,19 @@
+const DATA ={
+    Name :"Fesobi Oreofeoluwa",
+    Email :'fesobioreofe@gmail.com',
+    github: 'Asto0re',
+    github_url: "https://github.com/Astro0re",
+    Linkedin :'fesobi-oreofeoluwa',
+    Linkedin_url : "https://www.linkedin.com/in/fesobi-oreofeoluwa-5b3a98210/",
+    Twitter : 'FesobiO',
+    Twitter_url : "https://twitter.com/FesobiO",
+    Desctription : "" ,
+    Skills:{
+        Technical : {'HTML' : [],'CSS':[] ,'Python':[] ,'R':[]},
+        Soft : {'Communication':[],'Teamwork':[],'Problem-solving':[]}
+    },
+    Degree : 'Bsc Anatomy',
+    Certrifications : {},
+    Projects:{},
+    Experience:{}
+}
