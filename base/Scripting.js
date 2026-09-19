9ee5e34d30d4c9f52ad13ad1b45c1403637d1f89
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2026-09-19 15:42:11 +0100

    Django project removed, base portfolio restructured and edited

--- base/Scripting.js
+++ base/Scripting.js
@@ -1,47 +1,50 @@
 // Certification Display Function
 function cert_display_IBM(){
     document.getElementById('Cer_dis').innerHTML = `
+        <h4>Courses/Certifications</h4>
         <h5>Certification In Data Fundamentals from IBM SkillsBuild</h5>
         <h5>Certification In Big Data Fundamentals from IBM SkillsBuild</h5> 
         <h5>Certification In Artificail Intelligence Fundamentals from IBM SkillsBuild</h5>
-        <h6><a href="Certifications Display.html" style="text-align: center;">View all</a></h6>
+        
         
         `;
 }
 
 function cert_display_KAGG(){
     document.getElementById('Cer_dis').innerHTML = `
+            <h4>Courses/Certifications</h4>
             <h5>Certification in Introduction to Programming from Kaggle Learn</h5>
             <h5>Certification in Python from Kaggle Learn</h5>
             <h5>Certification in SQL from Kaggle Learn</h5>
-            <h6><a href="Certifications Display.html" style="text-align: center;">View all</a></h6>            
+                       
             `;
 }
 function cert_display_ALX(){
     document.getElementById('Cer_dis').innerHTML = `
+            <h4>Courses/Certifications</h4>
 			<h5>Certification in Ai Career Essentials from ALX</h5>
-            <h6><a href="Certifications Display.html" style="text-align: center;">View all</a></h6>
+            
             
             `;
 }
 function cert_display_SAN(){
     document.getElementById('Cer_dis').innerHTML = `
+            <h4>Courses/Certifications</h4>
 			<h5>Certification in High-Performance Leadership from Santander</h5>
-            <h6><a href="Certifications Display.html" style="text-align: center;">View all</a></h6>
             
             `;
 }
 function cert_display_AWS(){
     document.getElementById('Cer_dis').innerHTML = `
+                <h4>Courses/Certifications</h4>
                 <h5>Certification in Data Engineering from AWS Skills Build </h5>
-                <h6><a href="Certifications Display.html" style="text-align: center;">View all</a></h6>
                 
                 `;
 }
 function cert_display_OCI(){
     document.getElementById('Cer_dis').innerHTML = `
+                <h4>Courses/Certifications</h4>
                 <h5>Oracle Cloud Infrastructure 2025 Certified AI Foundations Associate</h5>
-                <h6><a href="Certifications Display.html" style="text-align: center;">View all</a></h6>
                 
                 `;
 }
